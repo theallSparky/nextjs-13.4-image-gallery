2765f352d618c11aec0b54864dd412c0a15f50cc
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-05 10:18:25 -0600

    Created a very basic 404 not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h1>Sorry, not found!</h1>
+            <h2>Sorry, this page does not exist!</h2>
+        </div>
+    )
+}
