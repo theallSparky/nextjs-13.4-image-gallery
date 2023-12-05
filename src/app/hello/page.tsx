a3ec5b52758fb27fac714f364c0c31267bad18cb
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-05 13:05:31 -0600

    Addded error in comment to test error page routing

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,4 +1,5 @@
 export default async function Page() {
     await new Promise((resolve) => setTimeout(resolve, 1000))
+    // throw Error("ErrorMcErrorface")
     return <div>Hello Next.js 13!</div>
 }
