c4d12f0c32b53a65354e1043e9a32c650f736fef
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-05 10:22:28 -0600

    Created a custom loading screen, added delay  for /hello

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,3 +1,4 @@
-export default function Page() {
+export default async function Page() {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
     return <div>Hello Next.js 13!</div>
 }
---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return(
+        <div>
+            <h1>Loading...Hold on to your horses!</h1>
+            <p>sheesh...</p>
+        </div>
+    )
+}
