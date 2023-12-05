e4dd906e713da3d54ebd9b5370874e8c5ca279d7
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-05 10:06:26 -0600

    Tiny reformat, added Bootstrap to description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Sparkys gallery App',
-  description: 'Next.js Typescript Gallery App',
+  description: 'Next.js Typescript Bootstrap Gallery App',
 }
 
 export default function RootLayout({
@@ -16,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div></div>
+        {children}
+      </body>
     </html>
   )
 }
