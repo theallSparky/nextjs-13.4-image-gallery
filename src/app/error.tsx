38d8e471577fc14984b656f9093f2ad3b2dee6df
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-05 12:37:51 -0600

    Created error page

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+
+export default function Error() {
+    return(
+        <div>
+            <h1>Error!</h1>
+            <p>Somewhere, somebody made an oopsie woopsie!</p>
+        </div>
+    )
+}
