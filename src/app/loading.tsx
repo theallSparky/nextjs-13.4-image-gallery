c4a48717453e2f8250c95a44ce11cff858779e02
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-06 22:19:25 -0600

    Stylized loading screen a bit more

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,9 +3,9 @@ import { Spinner } from "react-bootstrap";
 export default function Loading() {
     return(
         <div>
-            <Spinner />
-            <h1>Loading...Hold on to your horses!</h1>
-            <p>sheesh...</p>
+            <Spinner animation="border" className="d-block m-auto"/>
+            <h1 className="m-auto text-center py-3">Loading...Hold on to your horses!</h1>
+            <p className="m-auto text-center py-3">sheesh...</p>
         </div>
     )
 }
