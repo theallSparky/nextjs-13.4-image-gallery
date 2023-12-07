b7fec786b4e581c996827f7f7111d75e8372afcc
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-06 21:56:41 -0600

    Added spinner in loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,9 @@
+import { Spinner } from "react-bootstrap";
+
 export default function Loading() {
     return(
         <div>
+            <Spinner />
             <h1>Loading...Hold on to your horses!</h1>
             <p>sheesh...</p>
         </div>
