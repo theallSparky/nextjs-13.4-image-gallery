902314cce884bf81527faa9fa4a463f4ccc232c2
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-07 08:31:42 -0600

    Updated link in navbar

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,12 +1,17 @@
 "use client"
 
+import Link from "next/link"
 import { Container, Nav, Navbar } from "react-bootstrap"
 
 export default function NavBar() {
     return(
         <Navbar bg='primary' variant="dark" sticky='top' expand='sm' collapseOnSelect >
             <Container>
-                <Navbar.Brand href='/'>Next.js 13.4 Image Gallery</Navbar.Brand>
+                <Navbar.Brand>
+                    <Link href='/'>
+                        Next.js 13.4 Image Gallery
+                    </Link>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls='main-navbar' />
                 <Navbar.Collapse id='main-navbar' />
                     <Nav>
