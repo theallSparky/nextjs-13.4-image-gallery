d8981ee092f7c05344984de3c7d1918964eca8da
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-07 08:26:40 -0600

    Added navbar toggle and collapse

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -7,6 +7,8 @@ export default function NavBar() {
         <Navbar bg='primary' variant="dark" sticky='top' expand='sm' collapseOnSelect >
             <Container>
                 <Navbar.Brand href='/'>Next.js 13.4 Image Gallery</Navbar.Brand>
+                <Navbar.Toggle aria-controls='main-navbar' />
+                <Navbar.Collapse id='main-navbar' />
             </Container>
         </Navbar>
     )
