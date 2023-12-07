a4d32f494dffadb8d90f16c5dcd43a54538b5589
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-07 08:45:29 -0600

    Nav menu for navigation works

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
     return(
         <Navbar bg='primary' variant="dark" sticky='top' expand='sm' collapseOnSelect >
             <Container>
-                <Navbar.Brand as={Link}>
+                <Navbar.Brand as={Link} href='/'>
                     <Link href='/'>
                         Next.js 13.4 Image Gallery
                     </Link>
@@ -15,7 +15,7 @@ export default function NavBar() {
                 <Navbar.Toggle aria-controls='main-navbar' />
                 <Navbar.Collapse id='main-navbar' />
                     <Nav>
-                        <Nav.Link href='/hello'>Hello</Nav.Link>
+                        <Nav.Link as={Link} href='/hello'>Hello</Nav.Link>
                     </Nav>
             </Container>
         </Navbar>
