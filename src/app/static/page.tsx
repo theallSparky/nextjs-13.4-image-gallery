cdee5cf7776cf6cb39940571903c352789b6ae73
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-07 22:49:25 -0600

    Made page.tsx to load image

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,4 +1,22 @@
+import { UnsplashImage } from "@/models/unsplash-image"
+import Image from "next/image"
+
 export default async function Page() {
     const response = await fetch("https://api.unsplash.com/photos/random?client_id" + process.env.UNSPLASH_ACCESS_KEY)
-    
+    const image: UnsplashImage = await response.json()
+
+    const width = Math.min(500, image.width)
+    const height = (width / image.width) * image.height
+
+    return (
+        <div className="d-flex flex-column align-items-center">
+
+            <Image 
+                src={image.urls.raw}
+                width={width}
+                height={height}
+                alt={image.description}
+            />
+        </div>
+    )
 }
